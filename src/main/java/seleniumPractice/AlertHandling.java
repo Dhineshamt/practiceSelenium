37d7ef989f8a444e2ddd1b5d070dbd566e5543d7
a0135561 <elivision.11@gmail.com>
2018-03-12 12:18:08 +0530

    update

--- src/main/java/seleniumPractice/AlertHandling.java
+++ src/main/java/seleniumPractice/AlertHandling.java
@@ -2,9 +2,16 @@ package seleniumPractice;
 
 import util.LaunchPageInChrome;
 
+import java.security.NoSuchAlgorithmException;
+
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.UnhandledAlertException;
+import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
@@ -16,49 +23,56 @@ public class AlertHandling {
 	 * Reference website to learn Css selector - http://www.way2automation.com/selenium-webdriver-xpath-vs-css.php
 	 */
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		ChromeDriver driver = LaunchPageInChrome.LaunchPage("http://toolsqa.com/handling-alerts-using-selenium-webdriver/");
 		driver.manage().window().maximize();
 		
-		/*Css selector using id (#) and child (>)
-		 * Css explanation: * div -> node, * # -> ID, * content -> value of ID attribute,  * > child, * h1 child of div node
-		 * Ex:	<div id='content'>
-		 * 			<h1>Dhinesh</h1>
-		 * 		</div>
-		 * Css selector used below will select h1
-		 */	   
-		WebElement pageHeader = driver.findElementByCssSelector("div#content>h1");
+		WebElement simpleAlertButton = null, confirmAlertButton = null, promptAlertButton = null;
 		
-		/* Css selector using class. the dot '.' denotes the class
-		 * Css selector used below will select element with h2 tag
-		 * <div class='wf-container-main'>
-		 * 		<div id='content'>
-		 * 			<h2>I am dhinesh</h2>
-		 * 		</div>
-		 *	</div>
-		 */
-		WebElement pageContext = driver.findElementByCssSelector("div.wf-container-main>div#content>h2");
+		try {
+		//Webelement of simple alert
+		simpleAlertButton = driver.findElementByCssSelector("button[onclick='pushAlert()']");
+			
+		//Webelement of confirm alert box 
+		confirmAlertButton = driver.findElementByCssSelector("button[onclick^='pushCon']");
 		
+		//Webelement of prompt alert
+		promptAlertButton = driver.findElementByCssSelector("button[onclick$='Confirm()']");
+				
+		//Simple alert
+		simpleAlertButton.click();
+		Thread.sleep(3000);
+		Alert simpleAlert = driver.switchTo().alert();
+		simpleAlert.accept();
 		
-		/* Css Selector using attribute value
-		 * Syntax to get element by matching attribute and attribute value: "tag[attribute='attribute Value']"
-		 * Css Selector used below will select element with tag as button and onclick attribute value as 'pushAlert()'
-		 * <button onclick="pushAlert()">Simple Alert</button>
-		 */
-		WebElement simpleAlert = driver.findElementByCssSelector("button[onclick='pushAlert()']");
+		//Confirm alert
+		Thread.sleep(3000);
+		confirmAlertButton.click(); 
+		Thread.sleep(3000);
+		Alert confirmAlert = driver.switchTo().alert();
+		confirmAlert.dismiss();
 		
-		//Css selector with partial attribute where ^ demotes starts-with. 
-		WebElement confirmAlert = driver.findElementByCssSelector("button[onclick^='pushCon']");
-		//WebElement ;
+		//Prompt alert
+		promptAlertButton.click();
+		Thread.sleep(3000);
+		Alert promptAlert = driver.switchTo().alert();
+		promptAlert.sendKeys("Dhinesh");
+		promptAlert.accept();
 		
-		//Click Simple Alert
-		driver.findElementByXPath("//button[text()='Simple Alert']").click();
-		
-
-		
-		/*Actions build = new Actions(driver);
-		build.keyDown(Keys.DOWN).keyUp(Keys.DOWN).build().perform();*/
-		
+		} catch(NoSuchElementException e) {
+			System.out.println(e);
+		} catch(NoAlertPresentException e) {
+			System.out.println("No alert is present to click");
+			
+		} catch(UnhandledAlertException e) {
+			System.out.println("Please handle the exception");
+			
+		} catch(StaleElementReferenceException e) {
+			System.out.println("Element became invisible");
+			
+		} catch(WebDriverException e) {
+			System.out.println(e.getAdditionalInformation());
+		}
 		
 	}
 
--- src/main/java/seleniumPractice/CssSelector.java
+++ src/main/java/seleniumPractice/CssSelector.java
@@ -1,18 +1,65 @@
 package seleniumPractice;
 
-import org.openqa.selenium.chrome.ChromeDriver;
-
 import util.LaunchPageInChrome;
 
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
 public class CssSelector {
+	
+	/* CSS Selector Practice
+	 * @author - Dhinesh on 11-Mar-2018
+	 * Reference website to learn Css selector - http://www.way2automation.com/selenium-webdriver-xpath-vs-css.php
+	 */
 
 	public static void main(String[] args) {
-		ChromeDriver driver = LaunchPageInChrome.LaunchPage("https://www.google.co.in");
+		ChromeDriver driver = LaunchPageInChrome.LaunchPage("http://toolsqa.com/handling-alerts-using-selenium-webdriver/");
 		driver.manage().window().maximize();
 		
-		driver.findElementByCssSelector("input#lst-ib").sendKeys("Google");
+		/*Css selector using id (#) and child (>)
+		 * Css explanation: * div -> node, * # -> ID, * content -> value of ID attribute,  * > child, * h1 child of div node
+		 * Ex:	<div id='content'>
+		 * 			<h1>Dhinesh</h1>
+		 * 		</div>
+		 * Css selector used below will select h1
+		 */	   
+		WebElement pageHeader = driver.findElementByCssSelector("div#content>h1");
 		
-		driver.findElementByCssSelector("").clear();
+		/* Css selector using class. the dot '.' denotes the class
+		 * Css selector used below will select element with h2 tag
+		 * <div class='wf-container-main'>
+		 * 		<div id='content'>
+		 * 			<h2>I am dhinesh</h2>
+		 * 		</div>
+		 *	</div>
+		 */
+		WebElement pageContext = driver.findElementByCssSelector("div.wf-container-main>div#content>h2");
+		
+		
+		/* Css Selector using attribute value
+		 * Syntax to get element by matching attribute and attribute value: "tag[attribute='attribute Value']"
+		 * Css Selector used below will select element with tag as button and onclick attribute value as 'pushAlert()'
+		 * <button onclick="pushAlert()">Simple Alert</button>
+		 */
+		WebElement simpleAlert = driver.findElementByCssSelector("button[onclick='pushAlert()']");
+		
+		//Css selector with partial attribute where ^ demotes starts-with. 
+		WebElement confirmAlert = driver.findElementByCssSelector("button[onclick^='pushCon']");
+		//WebElement ;
+		
+		//Click Simple Alert
+		driver.findElementByXPath("//button[text()='Simple Alert']").click();
+		
+
+		
+		/*Actions build = new Actions(driver);
+		build.keyDown(Keys.DOWN).keyUp(Keys.DOWN).build().perform();*/
+		
+		
 	}
 
 }
